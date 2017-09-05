05438a8d9ad7ed6ebe7d615cdbd3a41ca4dc9283
Joel Richardson <joel.richardson@jax.org>
2017-09-05 15:04:10 -0400

    Better logfile naming. Reorganize some.

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -7,32 +7,40 @@ GUNZIP=gunzip
 GREP=grep
 SORT=sort
 MKDIR=mkdir
+DATE=date
+TOUCH=touch
+
+# The chromosomes and their order.
+CHRS=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 X Y MT )
 
 # https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BIN=${DIR}
+DATADIR=${DIR}/../data
+#DATADIR=/data/research/mouse_build_38_external/original_annotations
+WORKINGDIR=${DIR}/../work
+CACHEDIR=${DIR}/../cache
 PYTHONPATH=${DIR}/lib:${PYTHONPATH:-.}
 
 #
-BIN=${DIR}
-WORKINGDIR=${DIR}/../work
+DATESTAMP=`${DATE} +"%Y-%m-%d"`
+
+#
+SORTCMD="sort -k 1,1 -k 4n,4n -k 5nr,5nr"
+SPLITCMD="${PYTHON} ${BIN}/splitGff.py -d ${WORKINGDIR}"
+
+#
 ${MKDIR} -p ${WORKINGDIR}
-CACHEDIR=${DIR}/../cache
 ${MKDIR} -p ${CACHEDIR}
 
-DATADIR=${DIR}/../data
-#DATADIR=/data/research/mouse_build_38_external/original_annotations
-
-SORTCMD='sort -k 1,1 -k 4n,4n -k 5nr,5nr'
-SPLIT="${PYTHON} ${BIN}/splitGff.py -d ${WORKINGDIR}"
-
-LOGFILE=${WORKINGDIR}/LOG
-truncate -s 0 ${LOGFILE}
+LOGFILE=${WORKINGDIR}/LOG.${DATESTAMP}
+${TOUCH} ${LOGFILE}
 
 function logit {
     echo `date` $1 >> ${LOGFILE}
 }
 
-export DIR BIN PYTHONPATH WORKINGDIR CACHEDIR DATADIR LOGFILE SORT GREP SORTCMD SPLIT
+export DIR BIN PYTHONPATH WORKINGDIR CACHEDIR DATADIR LOGFILE SORT GREP SORTCMD SPLITCMD
 
 nargs=$#
 
@@ -42,6 +50,7 @@ doncbi=F
 doensembl=F
 domirbase=F
 domerge=F
+doexome=F
 
 until [ -z "$1" ]  # Until all parameters used up . . .
 do
@@ -64,6 +73,9 @@ do
     merge)
 	domerge=T
         ;;
+    exome)
+	doexome=T
+        ;;
     *)
         echo "Unrecognized option:" $1
 	exit -1
@@ -80,7 +92,7 @@ logit "Starting refresh..."
 # MGI
 if [ $nargs -eq 0 -o $domgi == T ]; then
     logit 'prepMgi...'
-    ${PYTHON} ${BIN}/prepMgi.py 2>> ${LOGFILE} | ${SORTCMD} | ${SPLIT} -t "mgi.chr%s.gff" 
+    ${PYTHON} ${BIN}/prepMgi.py 2>> ${LOGFILE} | ${SORTCMD} | ${SPLITCMD} -t "mgi.chr%s.gff" 
 fi
 
 ########
@@ -94,14 +106,14 @@ fi
 # NCBI
 if [ $nargs -eq 0 -o $doncbi == T ]; then
     logit 'prepNcbi...'
-    ${PYTHON} ${BIN}/prepNcbi.py 2>> ${LOGFILE} < ${DATADIR}/ref_GRCm38.p4_top_level.gff3 | ${SPLIT} -t "ncbi.chr%s.gff"
+    ${PYTHON} ${BIN}/prepNcbi.py 2>> ${LOGFILE} < ${DATADIR}/ref_GRCm38.p4_top_level.gff3 | ${SPLITCMD} -t "ncbi.chr%s.gff"
 fi
 
 ########
 # miRBase
 if [ $nargs -eq 0 -o $domirbase == T ]; then
     logit 'prepMirbase...'
-    ${PYTHON} ${BIN}/prepMirbase.py 2>> ${LOGFILE} < ${DATADIR}/miRBase21_mmu.gff3 | ${SPLIT} -t "mirbase.chr%s.gff"
+    ${PYTHON} ${BIN}/prepMirbase.py 2>> ${LOGFILE} < ${DATADIR}/miRBase21_mmu.gff3 | ${SPLITCMD} -t "mirbase.chr%s.gff"
 fi
 
 ########
@@ -113,19 +125,27 @@ if [ $nargs -eq 0 -o $doensembl == T ]; then
     #
     #${CURL} ${ENSEMBLurl} | ${GUNZIP} > ${DATADIR}/${ENSEMBLfile}
     logit 'prepEnsembl...'
-    ${PYTHON} ${BIN}/prepEnsembl.py 2>> ${LOGFILE} < ${DATADIR}/${ENSEMBLfile} | ${SPLIT} -t "ensembl.chr%s.gff"
+    ${PYTHON} ${BIN}/prepEnsembl.py 2>> ${LOGFILE} < ${DATADIR}/${ENSEMBLfile} | ${SPLITCMD} -t "ensembl.chr%s.gff"
 fi
 
 ########
 # MERGE phase
 if [ $nargs -eq 0 -o $domerge == T ]; then
-    chrs=( 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 X Y MT )
-    for i in "${chrs[@]}"
+    for i in "${CHRS[@]}"
     do : 
 	logit
 	logit "merging chr${i}..."
 	${PYTHON} ${BIN}/merge.py ${WORKINGDIR}/*.chr${i}.gff > ${WORKINGDIR}/chr${i}.gff 2>> ${LOGFILE}
     done
+    logit "catting files..."
+    cat ${WORKINGDIR}/chr*.gff | ${PYTHON} ${BIN}/reassignIDs.py > ${WORKINGDIR}/MGI.gff3 2>> ${LOGFILE}
+fi
+
+########
+# EXOME phase
+if [ $nargs -eq 0 -o $doexome == T ]; then
+    logit "creating MGI exome file..."
+    ${PYTHON} ${BIN}/exome.py ${WORKINGDIR}/MGI.gff3 > ${WORKINGDIR}/MGI.exome.gff3 2>> ${LOGFILE}
 fi
 
 ########
